f05874fc0426485506df35e4b2dd47198235c854
basir Development Team <dev@basir-mvp.local>
2026-07-20 19:56:30 +0300

    feat: add dark mode support for state colors and designs

--- lib/core/theme/selected_state_design.dart
+++ lib/core/theme/selected_state_design.dart
@@ -1,14 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:basir_accounting_system/core/theme/app_state_colors.dart';
 import 'package:basir_accounting_system/core/theme/tokens/index.dart';
-import 'package:flutter/material.dart';
 
 /// Selected state design utilities
 abstract final class SelectedStateDesign {
   /// Build a selected container decoration
-  static BoxDecoration buildSelectedBoxDecoration() => BoxDecoration(
-        color: AppStateColors.selectedBackground,
+  static BoxDecoration buildSelectedBoxDecoration([
+    Brightness brightness = Brightness.light,
+  ]) =>
+      BoxDecoration(
+        color: AppStateColors.getSelectedBackground(brightness),
         border: Border.all(
-          color: AppStateColors.selectedBorder,
+          color: AppStateColors.getSelectedBorder(brightness),
           width: 2,
         ),
         borderRadius: BorderRadius.circular(12),
@@ -22,9 +25,10 @@ abstract final class SelectedStateDesign {
     Widget? trailing,
     bool showCheckmark = true,
     VoidCallback? onTap,
+    Brightness brightness = Brightness.light,
   }) =>
       Container(
-        decoration: buildSelectedBoxDecoration(),
+        decoration: buildSelectedBoxDecoration(brightness),
         child: ListTile(
           title: title,
           subtitle: subtitle,
@@ -32,7 +36,7 @@ abstract final class SelectedStateDesign {
           trailing: showCheckmark
               ? Icon(
                   Icons.check_circle,
-                  color: AppColors.primary,
+                  color: AppStateColors.getSelectedForeground(brightness),
                 )
               : trailing,
           onTap: onTap,
@@ -42,13 +46,14 @@ abstract final class SelectedStateDesign {
   /// Build a selected bottom nav item wrapper
   static Widget buildSelectedNavItem({
     required Widget child,
+    Brightness brightness = Brightness.light,
   }) =>
       Container(
         decoration: BoxDecoration(
-          color: AppStateColors.selectedBackground,
+          color: AppStateColors.getSelectedBackground(brightness),
           borderRadius: BorderRadius.circular(20),
           border: Border.all(
-            color: AppStateColors.selectedBorder,
+            color: AppStateColors.getSelectedBorder(brightness),
             width: 2,
           ),
         ),
